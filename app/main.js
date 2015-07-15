cbbdb356636b13f81304c04f2c8c078b999826f8
Adam Burmister <adam.burmister@gmail.com>
2015-07-14 18:41:48 -0700

    Folders

--- app/main.js
+++ app/main.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+import 'fetch';
+import alt from './alt';
+import React from 'react';
+
+class MyApp extends React.Component {
+  render() {
+    return <p>Hello World</p>;
+  }
+}
+
+React.render(<MyApp />, document.body);
